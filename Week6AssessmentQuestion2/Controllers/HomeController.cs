03c56203b98a2adb2b7c55921bbd4636debadbeb
abiec013 <aec1020121@yahoo.com>
2017-10-30 16:05:24 -0400

    First commit

---
+++ Week6AssessmentQuestion2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Week6AssessmentQuestion2.Models;
+
+namespace Week6AssessmentQuestion2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            NorthwindEntities NorthwindDB = new NorthwindEntities();
+            List<Order> AllCustomers = NorthwindDB.Orders.ToList();
+            ViewBag.EmpList = AllCustomers;
+            
+            
+
+            return View();
+            
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
